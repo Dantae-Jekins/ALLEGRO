1143bb27c87809e3c59add3eee53bb92ee9ef86c
Dantae-Jekins <dantae.sona.jekins@gmail.com>
2021-08-24 20:19:44 -0300

    Initialized Repository

---
+++ logging.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int error(int code)
+{
+
+	printf("Sendo formado ainda");
+
+}
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <allegro5/allegro.h>
+#include <stdio.h>
+
+int main()
+{
+	ALLEGRO_DISPLAY *display = NULL;
+	if(!al_init())
+	{
+		return -1;
+	}
+
+	display =	al_create_display(720, 480);
+	getchar();
+
+	return 0;	
+}
+
+//
