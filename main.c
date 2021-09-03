3d6f39a42ef1fb5766abc441476ce4484f75c406
Murilo Pereira <dantae.sona.jekins@gmail.com>
2021-09-03 13:36:57 -0300

    Update no main.c e remoção de arquivos:
    Remoção de arquivos para teste e a adição do pseudocódigo inicial em
    main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+	// Teste do ALLEGRO
 	ALLEGRO_DISPLAY *display = NULL;
 	if(!al_init())
 	{
@@ -13,6 +14,33 @@ int main()
 	getchar();
 
 	return 0;	
-}
 
-//
+	/* Pesudocódigo inicial
+	
+	Setup
+	{
+		Inicialização do display
+		
+		Inicialização dos elementos
+		 - Personagem
+		 - Matriz dos mapas (uma para vários) 
+		 - Variáveis globais
+	
+	}
+
+	while (jogo rodando) - Limitar a velocidade do game (fps)
+	{
+
+		Atualizar o display
+
+		Responder a eventos
+			- Recebimento de inputs
+			- Analisar colisões
+	  
+		Os eventos são diversos e mudam conforme quais inputs/
+		quais colisões ocorrem.
+	}
+
+	Término do game.
+	*/
+}
