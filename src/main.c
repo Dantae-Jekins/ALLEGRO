7f456e609feda1ebb4794f3bd6d9ab9e5b73231d
Dantae Jekins <dantae.sona.jekins@gmail.com>
2021-10-21 16:52:11 -0300

    Arquivos reorganizados

---
+++ src/main.c
@@ -0,0 +1,135 @@
+//ALLEGRO
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+
+//STANDART LIBRARIES
+#include <stdio.h>
+#include <stdlib.h>
+
+//LOCAL FILES
+#include "objects.h"
+#include "setup.h"
+#include "mapper.h"
+#include "render.h"
+#include "mapper.h"
+
+int main(void)
+{
+  // !!!______SETUP______!!!
+  // Inicialização e análise de erro
+  printf("\nEXECUTANDO MAIN\n");
+  if (!setup())
+  {
+    printf("\nAbortando execução");
+    return -1;
+  }
+
+  // Inicialização das estruturas ALLEGRO
+  ALLEGRO_DISPLAY *display = al_create_display(width, height);
+  ALLEGRO_TIMER *timer = al_create_timer(1.0 / fps);
+  ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+  ALLEGRO_FONT *SANS18 =
+      al_load_font("fonts/OpenSans-Bold.ttf", 18, 0);
+
+  if (!display || !timer || !queue)
+  {
+    printf("\n Estruturas ALLEGRO não inicializadas");
+    return -1;
+  }
+
+  // Registro dos eventos ALLEGRO
+  al_register_event_source(queue, al_get_display_event_source(display));
+  al_register_event_source(queue, al_get_timer_event_source(timer));
+  al_register_event_source(queue, al_get_keyboard_event_source());
+
+  // Inicialização do jogo
+  bool rodando = true;
+  int timezin = 0;
+  //EXPERIMENTAL v !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  
+  int ID1 = genBox();
+  box[ID1].posx = 200;
+  box[ID1].posy = 280;
+  box[ID1].type = 1;
+
+  int ID2 = genBox();
+  box[ID2].posx = 400;
+  box[ID2].posy = 320;
+  box[ID2].type = 2;
+
+  //EXPERIMENTAL ^ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  
+  printf("\nSETUP COMPLETO\n");
+  while (rodando)
+  {
+    //Espera um evento
+    ALLEGRO_EVENT evento;
+    ALLEGRO_KEYBOARD_STATE keystate;
+    al_start_timer(timer);
+    al_get_keyboard_state(&keystate);
+    al_wait_for_event(queue, &evento);
+    if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+    {
+      rodando = false;
+    }
+    if (evento.type == ALLEGRO_EVENT_TIMER)
+    {
+      //renderiza a tela aqui
+      al_clear_to_color(al_map_rgb(0, 0, 0));
+      render_and_collide();
+      al_flip_display();
+
+      if (evento.type == ALLEGRO_EVENT_TIMER)
+      {
+        // checa o teclado
+        if (al_key_down(&keystate, ALLEGRO_KEY_RIGHT))
+          player.posx += speed;
+
+        if (al_key_down(&keystate, ALLEGRO_KEY_LEFT))
+          player.posx += -speed;
+
+        if (al_key_down(&keystate, ALLEGRO_KEY_UP))
+          player.posy += -speed;
+
+        if (al_key_down(&keystate, ALLEGRO_KEY_DOWN))
+          player.posy += speed;
+
+        if (timezin == 30)
+        {
+          player.oxygen -= 1;
+          timezin = 0;
+          printf("\33[2K oxygen: %d\n", player.oxygen);
+          printf("\33[2K posx:   %d\n", player.posx);
+          printf("\33[2K posy:   %d\n", player.posy);
+          printf("\33[3A");
+        } timezin++;
+        
+        // se o jogador morrer seu pc se fode
+        if (player.oxygen <= 0)
+        {
+          player.estado = 0;
+          int j = 2;
+          int *ptr;
+          for (int i = 0; i < j; i++)
+          {
+            for (int z = 0; z < 100; z++)
+            {
+              int *ptr = malloc(sizeof(int)*j);  
+            }
+            j+=1;
+          }
+        }
+      }
+    }
+  }
+
+  // Destruição das estruturas ALLEGRO
+  al_destroy_event_queue(queue);
+  al_destroy_display(display);
+  al_destroy_timer(timer);
+  al_uninstall_keyboard();
+  printf("\n");
+}
---
+++ src/mapper.h
@@ -0,0 +1,64 @@
+int jatoi(char *vec)
+{
+  int i = 0, j = 1;
+  int retorno = 0;
+  while (vec[i] != '\0')
+    i++;
+
+  while (i >= 0)
+  {
+    retorno += (vec[i] - 48) * j;
+    i--;
+    j *= 10;
+  }
+
+  return retorno;
+}
+
+int jatoi(char *vec)
+{
+  int i = 0, j = 1;
+  int retorno = 0;
+  while (vec[i] != '\0')
+    i++;
+
+  while (i >= 0)
+  {
+    retorno += (vec[i] - 48) * j;
+    i--;
+    j *= 10;
+  }
+
+  return retorno;
+}
+
+int carregarMatrix(int nivel)
+{
+  char str[2];
+  int i = 0, ch = 0;
+  char nome_arquivo[25];
+  if (nivel == 0)
+    strcpy(nome_arquivo, "../fases/fase1"); //fase 0
+  else
+    strcpy(nome_arquivo, "../fases/fase2"); //fase 1
+
+  FILE *fase = fopen(nome_arquivo, "r"); //Abre o arquivo
+
+  /////teste de leitura/////
+  if (fase == NULL)
+  {
+    printf("Não abriu o %s", nome_arquivo);
+    return 1;
+  }
+
+  while ((i < mapSize) && ((ch = getc(fase)) != EOF))
+  {
+    if (!(ch == 44) && !(ch == 10))
+    {
+      map[i++] = (ch - 48);
+    }
+  }
+
+  fclose(fase);
+  return 0;
+}
